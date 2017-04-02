83e43d0b8149a867753f93ab97a12f7f34963fcc
michael lee <keymikey54@yahoo.com>
2017-04-01 21:24:57 -0700

    added code for dataset of clustered stacked barchart

--- practice/src.js
+++ practice/src.js
@@ -1,5 +1,10 @@
+import * from "d3/d3.min.js";
+
 var printBacon = function() {
 	console.log("Bacon is healthy, don't believe the doctors!");
 }
+
+
+d3.select("body").append("p").text("New paragraph!");
 
 printBacon();
